831ae11af6e96e849744ff7097082fd80957f6fd
Nathaly Learmonth <learmonth.nathaly@gmail.com>
2022-06-23 23:48:10 -0300

    se logra mostrar img con eventos y funciones!

--- desafio entregable 4/firstDOM.js
+++ desafio entregable 4/firstDOM.js
@@ -1,34 +1,34 @@
-const photosSaved = [];
+// const photosSaved = [];
 
-class Photos {
-  constructor(name, size, section, date, id) {
-    this.name = name;
-    this.size = size;
-    this.section = section;
-    this.date = date;
-    this.id = id;
-  }
-}
+// class Photos {
+//   constructor(name, size, section, date, id) {
+//     this.name = name;
+//     this.size = size;
+//     this.section = section;
+//     this.date = date;
+//     this.id = id;
+//   }
+// }
 
-let searchButton = document.getElementsById("search");
+// let searchButton = document.getElementsById("search");
 
-function createPhoto() {
-  let photoUploaded;
-  let photoName = document.getElementById("photoName");
-  let photoSize = document.getElementById("photoSize");
-  let photoSection = document.getElementById("inputGroupSelect01");
-  let photoDate = new Date();
-  let photoId = photosSaved.length;
+// function createPhoto() {
+//   let photoUploaded;
+//   let photoName = document.getElementById("photoName");
+//   let photoSize = document.getElementById("photoSize");
+//   let photoSection = document.getElementById("inputGroupSelect01");
+//   let photoDate = new Date();
+//   let photoId = photosSaved.length;
 
-  photoUploaded = new Photos(photoName.value, photoSize.value, photoSection.value, photoDate, photoId);
+//   photoUploaded = new Photos(photoName.value, photoSize.value, photoSection.value, photoDate, photoId);
 
-  photosSaved.push(photoUploaded);
-}
+//   photosSaved.push(photoUploaded);
+// }
 
-function searchPhoto() {
-  photosSaved.forEach((element, index) => {
-    alert(
-      `Detalles de tus fotos: \nNombre: ${element.name}, tamaño: ${element.size}, sección: ${element.section}, fecha: ${element.date} \nID: ${element.id}. `
-    );
-  });
-}
+// function searchPhoto() {
+//   photosSaved.forEach((element, index) => {
+//     alert(
+//       `Detalles de tus fotos: \nNombre: ${element.name}, tamaño: ${element.size}, sección: ${element.section}, fecha: ${element.date} \nID: ${element.id}. `
+//     );
+//   });
+// }
---
+++ desafio entregable 5/eventos.js
@@ -0,0 +1,70 @@
+const photosSaved = [];
+
+class Photos {
+  constructor(name, size, section, date, id) {
+    this.name = name;
+    this.size = size;
+    this.section = section;
+    this.date = date;
+    this.id = id;
+  }
+}
+
+let saveBtn = document.getElementById("saveBtn");
+let searchBtn = document.getElementById("searchBtn")
+
+const date = new Date();
+let formatDate = date.getDate() + " / " + (date.getMonth() + 1) + " / " + date.getFullYear();
+
+const fileToUpload = document.querySelector('input[type=file]');
+let divContainer = document.getElementById("uploadPhoto")
+
+saveBtn.addEventListener("click", createPhoto)
+searchBtn.addEventListener("click", addFileToDocument)
+
+function addFileToDocument() {
+  let photoName = document.getElementById("photoName");
+  let photoSize = document.getElementById("photoSize");
+  let photoSection = document.getElementById("inputGroupSelect01");
+  let photoDate = formatDate;
+  let photoId = photosSaved.length;
+  
+  photosSaved.forEach((e,index) => {
+    let imgContainer = document.createElement("div")
+    divContainer.append(imgContainer)
+    let name = document.createElement("h2")
+    name.innerText = (`Nombre: ${photoName.value}`)
+
+    const img = new Image()
+    let reader = new FileReader();
+      reader.onload = function(e) {
+        img.src = e.target.result;
+      };
+    reader.readAsDataURL(fileToUpload.files[index])
+    
+    let size = document.createElement("p")
+    size.innerText = (`Tamaño: ${photoSize.value} KB`)
+    let section = document.createElement("h3")
+    section.innerText = (`Sección: ${photoSection.value}`)
+    let date = document.createElement("h4")
+    date.innerText = (`Fecha: ${photoDate}`)
+    let id = document.createElement("h5")
+    id.innerText = (`ID: ${photoId}`)
+
+    imgContainer.append(img, name, size, section, date, id)
+  });
+  // console.log(fileToUpload.files)
+}
+
+
+function createPhoto() {
+  let photoUploaded;
+  let photoName = document.getElementById("photoName");
+  let photoSize = document.getElementById("photoSize");
+  let photoSection = document.getElementById("inputGroupSelect01");
+  let photoDate = formatDate;
+  let photoId = photosSaved.length;
+
+  photoUploaded = new Photos(photoName.value, photoSize.value, photoSection.value, photoDate, photoId);
+  photosSaved.push(photoUploaded);
+}
